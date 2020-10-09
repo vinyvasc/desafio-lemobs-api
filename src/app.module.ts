56b14fc769c709fa0615297d3927fc2599badfb4
Vinicius <vinivascdev@gmail.com>
2020-10-09 10:10:16 -0300

    importação das modules alunos e enderecos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './configs/typeorm.config';
+import { AlunosModule } from './alunos/alunos.module';
+import { EnderecosModule } from './enderecos/enderecos.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), AlunosModule, EnderecosModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
